6e3181f7ec1619038298443353f477fea11a6a0c
Harsh <harsh97830544@gmail.com>
2026-02-25 09:16:53 +0530

    DAY 25

---
+++ day25.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Node structure
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Create linked list
+struct Node* createList(int n) {
+    struct Node *head = NULL, *temp = NULL, *newNode;
+
+    for(int i = 0; i < n; i++) {
+        int value;
+        scanf("%d", &value);
+
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode->data = value;
+        newNode->next = NULL;
+
+        if(head == NULL) {
+            head = newNode;
+            temp = newNode;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+    return head;
+}
+
+// Count occurrences of key
+int countKey(struct Node* head, int key) {
+    int count = 0;
+    struct Node* temp = head;
+
+    while(temp != NULL) {
+        if(temp->data == key)
+            count++;
+        temp = temp->next;
+    }
+    return count;
+}
+
+int main() {
+    int n, key;
+
+    scanf("%d", &n);
+    struct Node* head = createList(n);
+
+    scanf("%d", &key);
+
+    int result = countKey(head, key);
+
+    printf("%d", result);
+    return 0;
+}
